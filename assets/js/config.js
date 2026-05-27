e17f158c59f1604df179c38e26dadcdd226a1759
Hugo Medina Rivas <60479842+HuGhOsTmR@users.noreply.github.com>
2026-05-26 22:24:31 -0400

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+
+//export const API_URL = "http://127.0.0.1:8000";
+// en producción:
+export const API_URL = "https://votapp-backend-g856.onrender.com/";
+
+//export const WS_URL = "ws://127.0.0.1:8000";
+// en producción:
+export const WS_URL = "wss://votapp-backend-g856.onrender.com";
