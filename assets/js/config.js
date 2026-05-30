abdc132baa9fac8da44d451949062000602411e9
Hugo Medina Rivas <60479842+HuGhOsTmR@users.noreply.github.com>
2026-05-29 23:58:02 -0400

    Fix API_URL export by removing trailing slash

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 
 //export const API_URL = "http://127.0.0.1:8000";
 // en producción:
-export const API_URL = "https://votapp-backend-g856.onrender.com/";
+export const API_URL = "https://votapp-backend-g856.onrender.com";
 
 //export const WS_URL = "ws://127.0.0.1:8000";
 // en producción:
